381eda6c962e703c74fccfa5a9a8ade58164d3f4
TanmayVyavahare <158759902+TanmayVyavahare@users.noreply.github.com>
2024-12-31 18:26:45 +0530

    Time: 14 ms (81.21%), Space: 47.3 MB (32.34%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,16 +1,40 @@
 class Solution {
+    private:
+    vector<int> findnge(vector<int>& arr){
+        int n= arr.size();
+        vector<int>nge(n);
+        stack<int> st;
+        for(int i = n-1;i>=0;i-- ){
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            nge[i] = st.empty()?n:st.top();
+            st.push(i);
+        }
+        return nge;
+    }
+    private:
+    vector<int> findpsee(vector<int>& arr){
+        int n= arr.size();
+        vector<int>psee(n);
+        stack<int> st;
+        for(int i = 0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+            psee[i] = st.empty()?-1:st.top();
+            st.push(i);
+        }
+        return psee;
+    }
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int mod = (int)1e9+7;
         int n = arr.size();
-        int sum = 0;
-        for(int i = 0 ;i<n;i++){
-            int mini = arr[i];
-            for(int j = i;j<n;j++){
-                mini = min (mini,arr[j]);
-                sum = (sum+mini)%mod;
-            }
+        int mod =(int) 1e9+7;
+        vector<int>nge =findnge(arr);
+        vector<int>psee =findpsee(arr);
+        long total = 0;
+        for(int i =0;i<n;i++){
+            int left = i-psee[i];
+            int right = nge[i]-i;
+            total = (total +(left*right*1LL*arr[i])%mod)%mod;
         }
-        return sum ;
+        return total ;
     }
 };
