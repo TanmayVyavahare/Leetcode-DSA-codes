b0b376cc97e03806c4b3004a249174d62410ce69
TanmayVyavahare <158759902+TanmayVyavahare@users.noreply.github.com>
2025-01-03 22:06:13 +0530

    Time: 0 ms (100%), Space: 15.8 MB (34.28%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,15 +2,61 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long sum=0;
-        for(int i =0;i<n;i++){
-            int maxval = nums[i],minval= nums[i];
-            for(int j =i;j<n;j++){
-                maxval = max(maxval,nums[j]);
-                minval = min(minval,nums[j]);
-                sum +=(maxval-minval);
+        vector<int> pse(n), nse(n), pge(n), nge(n);
+        stack<int> st;
+        
+        //find previous smaller or equal elements
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
             }
+            if(st.empty()) pse[i] = -1;
+            else pse[i] = st.top();
+            st.push(i);
         }
-        return sum;
+        //clear the stack
+        while(!st.empty()) st.pop();
+
+        //find previous greater or equal elements
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                st.pop();
+            }
+            if(st.empty()) pge[i] = -1;
+            else pge[i] = st.top();
+            st.push(i);
+        }
+
+        //clear the stack
+        while(!st.empty()) st.pop();
+
+        //find next smaller elements
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                st.pop();
+            }
+            if(st.empty()) nse[i] = n;
+            else nse[i] = st.top();
+            st.push(i);
+        }
+
+         //clear the stack
+        while(!st.empty()) st.pop();
+
+        //find next greater elements
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                st.pop();
+            }
+            if(st.empty()) nge[i] = n;
+            else nge[i] = st.top();
+            st.push(i);
+        }
+
+        long long res = 0;
+        for(int i = 0; i < n; i++){
+            res = res + (long long)(nums[i]*(long long)((i - pge[i]) *(nge[i] - i))) -  (long long)(nums[i]*(long long)((nse[i] - i) * (i - pse[i])));
+        }
+        return res;
     }
 };
